19cbc84a807a7e45800ebaca283df6a9221e45b6
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2023-11-15 21:42:55 +0100

    add content and feedback to summaryPage

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -2,15 +2,20 @@
 
 import { useSelector } from "react-redux";
 
-
 export const ProgressBar = () => {
   const totalQuestions = useSelector((state) => state.quiz.questions.length);
-  const currenQuestion = useSelector((state) => state.quiz.currentQuestionIndex)
-  console.log("total lenght",totalQuestions)
+  const currenQuestion = useSelector(
+    (state) => state.quiz.currentQuestionIndex
+  );
+  //console.log("total lenght",totalQuestions)
   return (
     <div>
-      <progress  value={currenQuestion+1} max={totalQuestions}>  </progress>
-      <span>{currenQuestion+1}/{totalQuestions}</span>
+      <progress value={currenQuestion + 1} max={totalQuestions}>
+        {" "}
+      </progress>
+      <span>
+        {currenQuestion + 1}/{totalQuestions}
+      </span>
     </div>
   );
 };
--- src/pages/SummaryPage.jsx
+++ src/pages/SummaryPage.jsx
@@ -1,19 +1,43 @@
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { ScoreCounter } from "../components/ScoreCounter";
 import { restart } from "../reducers/quiz";
+import "./summaryPage.css";
 
 export const SummaryPage = () => {
   const dispatch = useDispatch();
+
+  const answers = useSelector((state) => state.quiz.answers);
+
+  const questions = useSelector((state) => state.quiz.questions);
 
   const handleShowSummary = () => {
     dispatch(restart());
   };
   return (
     <div className="summaryContainer">
-      <h1>You finished the quiz, well done!</h1>
+      <h1>You finished the quiz, well done! 🏆</h1>
       <ScoreCounter />
-      <p>Correct answers: </p>
-      <p>Incorrect answers: </p>
+      <ul>
+        {answers.map(({ questionId, isCorrect }) => (
+          <li key={questionId}>
+            <p className={isCorrect ? "green" : "red"}>
+              {`
+              ${isCorrect ? "✅" : "❌"}
+              ${questionId}: 
+              ${
+                questions.filter(({ id }) => id === questionId)[0].questionText
+              } - 
+              ${
+                isCorrect
+                  ? "You answered correct!"
+                  : "You answered incorrect 😞"
+              }
+              `}
+            </p>
+          </li>
+        ))}
+      </ul>
+
       <button onClick={handleShowSummary}>Try again?</button>
     </div>
   );
